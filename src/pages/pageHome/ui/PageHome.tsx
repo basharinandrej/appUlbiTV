a107890c5d5e013976a20d501b73f500780c7794
Basharin Andrey <a.basharin@okko.tv>
2023-04-10 08:59:31 +0300

    added mocks

--- src/pages/pageHome/ui/PageHome.tsx
+++ src/pages/pageHome/ui/PageHome.tsx
@@ -1,10 +1,15 @@
 import { useTranslation } from 'react-i18next'
+import {Counter} from "@entities/counter/ui/counter";
+import {Fragment} from "react";
 
 const PageHome = () => {
     const { t } = useTranslation()
 
     return (
-        <h1>{t('Home')}</h1>
+        <Fragment>
+            <h1>{t('Home')}</h1>
+            <Counter />
+        </Fragment>
     )
 }
 
