50b62ddbcf45e5c2cbb3c4cda3e9d7554d95656f
Basharin Andrey <a.basharin@okko.tv>
2023-07-02 12:59:22 +0300

    fix conflict

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -6,18 +6,10 @@ import {MainLayout} from '@widgets/MainLayout'
 import {AppRoutes} from '@app/providers/AppRoutes'
 import {PageLoader} from '@widgets/PageLoader/ui/PageLoader'
 import {PageLoaderType} from '@widgets/PageLoader/types/enums/page-loader-type'
-import {useDispatch} from "react-redux";
-import { initUser } from '@entities/user'
-import {useMount} from "@shared/libs/hooks/useMount";
 
 
 function App() {
     const {theme} = useTheme()
-    const dispatch = useDispatch()
-
-    useMount(() => {
-        dispatch(initUser())
-    })
 
     return (
         <Suspense fallback={<PageLoader type={PageLoaderType.SECONDARY}/>}>
