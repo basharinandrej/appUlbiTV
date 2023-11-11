f009c6c298a57aced2b08955e7c3394a76cd63f7
a.basharin <a.basharin@okko.tv>
2023-11-11 20:01:21 +0300

    add two comments for article details

--- src/entities/comment/ui/comment.tsx
+++ src/entities/comment/ui/comment.tsx
@@ -3,10 +3,13 @@ import {Avatar, setClassNames, SizeAvatar} from "@shared/index";
 
 import styles from './comment.module.sass'
 
-const avatarSrc = 'https://cdn4.iconfinder.com/data/icons/profession-and-occupation-3/512/IT_specialist-occupation-avatar-job-character-profession-512.png'
-
 export const Comment: VFC<commentProps> = (props) => {
-  const {className} = props
+  const {
+    name,
+    avatarSrc,
+    textComment,
+    className
+  } = props
 
   return (
       <div className={setClassNames(styles.comment, {}, [className])}>
@@ -18,17 +21,20 @@ export const Comment: VFC<commentProps> = (props) => {
               className={styles.avatar}
           />
           <p className={styles.text}>
-            Андрей Башарин
+            {name}
           </p>
         </div>
 
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, quaerat!
+          {textComment}
         </p>
       </div>
   )
 }
 
 interface commentProps {
+  name: string
+  avatarSrc: string
+  textComment: string
   className?: string
 }
--- src/entities/comment/ui/stories/comment.stories.tsx
+++ src/entities/comment/ui/stories/comment.stories.tsx
@@ -1,6 +1,8 @@
 import {Comment} from '@entities/comment'
 import {ComponentStory} from "@storybook/react";
 
+const avatarSrc = 'https://cdn4.iconfinder.com/data/icons/profession-and-occupation-3/512/IT_specialist-occupation-avatar-job-character-profession-512.png'
+
 export default {
   title: 'ENTITIES/comment',
   component: Comment
@@ -9,3 +11,8 @@ export default {
 const CommentTemplate: ComponentStory<typeof Comment> = (args) => <Comment {...args} />
 
 export const CommentPrimary = CommentTemplate.bind({}) as typeof CommentTemplate
+CommentPrimary.args = {
+  name: 'Башарин Андрей',
+  textComment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, natus!',
+  avatarSrc,
+}
