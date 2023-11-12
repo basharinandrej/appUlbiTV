6086f3b1b7f359b629c044c0fc2b7f85610c4e0f
a.basharin <a.basharin@okko.tv>
2023-11-12 16:33:33 +0300

    add extraReducer for comments listing

--- src/entities/article/model/slice/articleDetailsSlice.ts
+++ src/entities/article/model/slice/articleDetailsSlice.ts
@@ -2,8 +2,6 @@ import {Nullable} from '@shared/index'
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import {fetchArticleDetailsById} from '../asyncAction/fetchArticleDetailsById'
 import {Article} from '../types/article'
-import {sendNewComment} from '@features/AddNewComment/model/asyncAction/sendNewComment'
-import {IComment} from '@entities/comment'
 
 export interface ArticleDetailsSchema {
     data: Nullable<Article>
@@ -35,19 +33,6 @@ const articleDetailsSlice = createSlice({
                 state.isLoading = false
                 state.error = action.payload.msg
             })
-
-            .addCase(sendNewComment.fulfilled, (state: ArticleDetailsSchema, action: PayloadAction<IComment>) => {
-                const updatedComments = [action.payload].concat(state.data.comments)
-
-                return {
-                    ...state,
-                    data: {
-                        ...state.data,
-                        comments: updatedComments
-                    }
-                }
-            })
-
     }
 })
 
--- src/features/CommentsListing/model/slice/commentsListingSlice.ts
+++ src/features/CommentsListing/model/slice/commentsListingSlice.ts
@@ -2,6 +2,7 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {CommentsListingSliceSchema} from "../types/types";
 import {fetchCommentsByArticleId} from "@features/CommentsListing/model/asyncActions/fetchCommentsByArticleId";
 import {IComment} from "@entities/comment";
+import {sendNewComment} from "@features/AddNewComment/model/asyncAction/sendNewComment";
 
 
 const initialState: CommentsListingSliceSchema = {
@@ -16,6 +17,19 @@ const commentsListingSlice = createSlice({
     builder
       .addCase(fetchCommentsByArticleId.fulfilled, (state, action:PayloadAction<IComment[]>) => {
         state.articles = action.payload
+      })
+      .addCase(sendNewComment.fulfilled, (state: any, action: PayloadAction<IComment>) => {
+        const isArticles = action.payload.articleId
+
+        if(isArticles) {
+          return {
+            ...state,
+            articles: [action.payload, ...state.articles]
+          }
+        } else {
+          return state
+        }
+
       })
   }
 })
