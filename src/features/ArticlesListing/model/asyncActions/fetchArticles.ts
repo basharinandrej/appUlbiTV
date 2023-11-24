954d2684147ab03a0fa07a63bafb4cf6a5964d50
a.basharin <a.basharin@okko.tv>
2023-11-24 21:06:21 +0300

    add addQueryParams

--- src/features/ArticlesListing/model/asyncActions/fetchArticles.ts
+++ src/features/ArticlesListing/model/asyncActions/fetchArticles.ts
@@ -3,6 +3,19 @@ import {Article} from '@entities/article'
 import {ThunkApiConfig} from '@app/providers/StoreProvider'
 import { getTypeSort, getOrderSort, getSearch } from '@features/FiltersForArticlesListing/model/selectors/selectors'
 
+const addQueryParams = (params: Record<string, string>) => {
+  const searchParams = new URLSearchParams(window.location.search)
+  Object.entries(params).forEach(([key, value]) => {
+    if(value) {
+      searchParams.set(key, value)
+    } else {
+      searchParams.delete(key)
+    }
+  })
+  window.history.pushState(null, '',`?${searchParams.toString()}`)
+}
+
+
 export const fetchArticles = createAsyncThunk<Array<Article>, void, ThunkApiConfig<string>>(
     'articles/fetchArticles',
     async (_, thunkAPI ) => {
@@ -12,6 +25,12 @@ export const fetchArticles = createAsyncThunk<Array<Article>, void, ThunkApiConf
       const typeSort = getTypeSort(state)
       const orderSort = getOrderSort(state)
       const search = getSearch(state)
+
+      addQueryParams({
+        _sort: typeSort,
+        _order: orderSort,
+        q: search
+      })
 
       try {
         const response = await extra.api.get<Array<Article>>('/articles', {
